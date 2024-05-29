6d945a308a856aa691fb3801c5302c3130c015f8
James Geib <james@zuse.com>
2024-05-29 14:41:25 -0400

    made adjustments to AnnouncementBar styling

--- client/src/components/AnnouncementBar/index.js
+++ client/src/components/AnnouncementBar/index.js
@@ -8,10 +8,10 @@ const AnnouncementBar = () => {
     return (
         <div id="announcement-container">
             <div className="announce-col">
-                <Header id="announce-phone-number" as="h4"><Icon id="announce-phone-icon" name="phone" />Call: 203-738-8106</Header>
+                <Header id="announce-address" as="h4">Can you dig it? Cause WE can!</Header>
             </div>
             <div className="announce-col">
-                <Header id="announce-address" as="h4"><Icon id="announce-map-icon" name="map pin" />1323 Ave D Unit 1129, Snohomish, WA, 98291</Header>
+                <Header id="announce-phone-number" as="h4">203-738-8106</Header>
             </div>
         </div>
     );
