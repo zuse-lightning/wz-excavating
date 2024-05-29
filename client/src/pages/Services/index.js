c9f5bde8332f47975e723c375178e93e0aa57e11
James Geib <james@zuse.com>
2024-05-29 10:58:18 -0400

    created subroutes for Services

--- client/src/pages/Services/index.js
+++ client/src/pages/Services/index.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const Services = (props) => {
     return (
         <>
-            <h1>Services</h1>
+            <Routes>
+                <Route exact path="top-soil" />
+                <Route exact path="gravel" />
+                <Route exact path="land-grading" />
+                <Route exact path="excavation" />
+                <Route exact path="drainage" />
+                <Route exact path="patios" />
+                <Route exact path="lawn-care" />
+                <Route exact path="snow-removal" />
+            </Routes>
         </>
     );
 };
