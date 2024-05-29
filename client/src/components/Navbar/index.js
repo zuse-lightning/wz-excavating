c10d822092bdeb7d35e133ccfd4e8c9316b5e9f4
James Geib <james@zuse.com>
2024-05-29 12:17:00 -0400

    successfully connected to AWS bucket and added site favicon

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Menu, Dropdown, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+import "./style.css";
+
+const Navbar = () => {
+    return (
+        <Menu id="main-menu" inverted pointing secondary fluid>
+            
+        </Menu>
+    );
+};
+
+export default Navbar;
