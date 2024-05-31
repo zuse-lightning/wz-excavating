92983bc2840308a43ffec26ba5d8346d0abeadb1
James Geib <james@zuse.com>
2024-05-31 15:59:15 -0400

    implemented subroutes for Service page

--- client/src/pages/Services/index.js
+++ client/src/pages/Services/index.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import TopSoil from "./TopSoil";
+import Gravel from "./Gravel";
+import LandGrading from "./LandGrading";
+import Excavation from "./Excavation";
+import Drainage from "./Drainage";
+import Patios from "./Patios";
+import LawnCare from "./LawnCare";
+import SnowRemoval from "./SnowRemoval";
+import SitePrep from "./SitePrep";
+import Driveways from "./Driveways";
+import Landscape from "./Landscape";
 
 const Services = (props) => {
     return (
         <>
             <Routes>
-                <Route exact path="top-soil" />
-                <Route exact path="gravel" />
-                <Route exact path="land-grading" />
-                <Route exact path="excavation" />
-                <Route exact path="drainage" />
-                <Route exact path="patios" />
-                <Route exact path="lawn-care" />
-                <Route exact path="snow-removal" />
-                <Route exact path="site-prep" />
-                <Route exact path="driveways" />
-                <Route exact path="landscape" />
+                <Route exact path="top-soil" element={<TopSoil />} />
+                <Route exact path="gravel" element={<Gravel />} />
+                <Route exact path="land-grading" element={<LandGrading />} />
+                <Route exact path="excavation" element={<Excavation />} />
+                <Route exact path="drainage" element={<Drainage />} />
+                <Route exact path="patios" element={<Patios />} />
+                <Route exact path="lawn-care" element={<LawnCare />} />
+                <Route exact path="snow-removal" element={<SnowRemoval />} />
+                <Route exact path="site-prep" element={<SitePrep />} />
+                <Route exact path="driveways" element={<Driveways />} />
+                <Route exact path="landscape" element={<Landscape />} />
             </Routes>
         </>
     );
