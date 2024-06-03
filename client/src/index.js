eec4d87c4bbe37207f6e817c66d4dd889f0c232b
James Geib <james@zuse.com>
2024-06-03 14:51:21 -0400

    constructed and implemented ScrollToTop module

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>
 );
