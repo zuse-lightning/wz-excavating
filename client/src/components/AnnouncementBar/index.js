501fb8b0dba633f4cc749b51f663612aef1fc4d4
James Geib <james@zuse.com>
2024-06-10 14:11:23 -0400

    made slight cosmetic change to AnnouncementBar text

--- client/src/components/AnnouncementBar/index.js
+++ client/src/components/AnnouncementBar/index.js
@@ -8,7 +8,7 @@ const AnnouncementBar = () => {
     return (
         <div id="announcement-container">
             <div className="announce-col">
-                <Header id="announce-address" as="h4">Can you dig it? Cause WE can!</Header>
+                <Header id="announce-address" as="h4">Can <i>you</i> dig it? Cause WE can!</Header>
             </div>
             <div className="announce-col">
                 <Header id="announce-phone-number" as="h4">203-738-8106</Header>
