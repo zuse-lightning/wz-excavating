c7dadf93f8469b585e53eaa1a5b8a0cecb08168b
James Geib <james@zuse.com>
2024-06-12 13:55:56 -0400

    created initial structure and styling for QuoteForm component

--- client/src/components/QuoteForm/index.js
+++ client/src/components/QuoteForm/index.js
@@ -2,41 +2,41 @@ import React from "react";
 
 import "./style.css";
 
-const ContactForm = (props) => {
+const QuoteForm = (props) => {
 
     return (
         <>
-            <form id="contact-form">
-                <div className="contact-form-col">
+            <form id="quote-form">
+                <div className="quote-form-col">
                     <label className="form-label" id="name-label" htmlFor="name">Name:</label>
                 </div>
-                <div className="contact-form-col">
+                <div className="quote-form-col">
                     <input type="text" className="form-input" id="name-input" name="name" />
                 </div>
-                <div className="contact-form-col">
+                <div className="quote-form-col">
                     <label className="form-label" id="email-label" htmlFor="email">Email:</label>
                 </div>
-                <div className="contact-form-col">
+                <div className="quote-form-col">
                     <input type="email" className="form-input" id="email-input" name="email" />
                 </div>
-                <div className="contact-form-col">
+                <div className="quote-form-col">
                     <label className="form-label" id="phone-label" htmlFor="email">Phone:</label>
                 </div>
-                <div className="contact-form-col">
+                <div className="quote-form-col">
                     <input type="text" className="form-input" id="phone-input" name="phone" />
                 </div>
-                <div className="contact-form-col">
+                <div className="quote-form-col">
                     <label className="form-label" id="message-label" htmlFor="message">Message:</label>
                 </div>
-                <div className="contact-form-col">
+                <div className="quote-form-col">
                     <textarea className="form-input" id="message-input" name="message"></textarea>
                 </div>
-                <div className="contact-form-col">
-                    <button id="contact-submit-btn" type="submit">Submit</button>
+                <div className="quote-form-col">
+                    <button id="quote-submit-btn" type="submit">Submit</button>
                 </div>
             </form>
         </>
     );
 }
 
-export default ContactForm;
+export default QuoteForm;
