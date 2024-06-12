948851828ac17741ad45604165f549cb323e8810
James Geib <james@zuse.com>
2024-06-12 15:10:54 -0400

    figured out mvp structure for service checkboxes in quote form

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -11,16 +11,16 @@ const ContactForm = (props) => {
                     <label className="form-label" id="name-label" htmlFor="name">Name:</label>
                 </div>
                 <div className="contact-form-col">
-                    <input type="text" className="form-input" id="name-input" name="name" />
+                    <input type="text" className="form-input" id="name-input" name="name" required />
                 </div>
                 <div className="contact-form-col">
                     <label className="form-label" id="email-label" htmlFor="email">Email:</label>
                 </div>
                 <div className="contact-form-col">
-                    <input type="email" className="form-input" id="email-input" name="email" />
+                    <input type="email" className="form-input" id="email-input" name="email" required />
                 </div>
                 <div className="contact-form-col">
-                    <label className="form-label" id="phone-label" htmlFor="email">Phone:</label>
+                    <label className="form-label" id="phone-label" htmlFor="phone">Phone:</label>
                 </div>
                 <div className="contact-form-col">
                     <input type="text" className="form-input" id="phone-input" name="phone" />
@@ -29,7 +29,7 @@ const ContactForm = (props) => {
                     <label className="form-label" id="message-label" htmlFor="message">Message:</label>
                 </div>
                 <div className="contact-form-col">
-                    <textarea className="form-input" id="message-input" name="message"></textarea>
+                    <textarea className="form-input" id="message-input" name="message" required></textarea>
                 </div>
                 <div className="contact-form-col">
                     <button id="contact-submit-btn" type="submit">Submit</button>
--- client/src/components/QuoteForm/index.js
+++ client/src/components/QuoteForm/index.js
@@ -11,25 +11,74 @@ const QuoteForm = (props) => {
                     <label className="form-label" id="name-label" htmlFor="name">Name:</label>
                 </div>
                 <div className="quote-form-col">
-                    <input type="text" className="form-input" id="name-input" name="name" />
+                    <input type="text" className="form-input" id="name-input" name="name" required />
+                </div>
+                <div className="quote-form-col">
+                    <label className="form-label" id="company-name-label" htmlFor="company-name">Company:</label>
+                </div>
+                <div className="quote-form-col">
+                    <input type="text" className="form-input" id="name-input" name="company-name" />
+                </div>
+                <div className="quote-form-col">
+                    <label className="form-label" id="address-label" htmlFor="address">Address:</label>
+                </div>
+                <div className="quote-form-col">
+                    <input type="text" className="form-input" id="address-input" name="address" required />
                 </div>
                 <div className="quote-form-col">
                     <label className="form-label" id="email-label" htmlFor="email">Email:</label>
                 </div>
                 <div className="quote-form-col">
-                    <input type="email" className="form-input" id="email-input" name="email" />
+                    <input type="email" className="form-input" id="email-input" name="email" required />
                 </div>
                 <div className="quote-form-col">
-                    <label className="form-label" id="phone-label" htmlFor="email">Phone:</label>
+                    <label className="form-label" id="phone-label" htmlFor="phone">Phone:</label>
                 </div>
                 <div className="quote-form-col">
                     <input type="text" className="form-input" id="phone-input" name="phone" />
                 </div>
                 <div className="quote-form-col">
+                    <label className="form-label" id="service-label" htmlFor="service">Service:</label>
+                </div>
+                <div className="quote-form-col">
+                    <div className="form-input checkbox-list">
+                        <div className="form-checkbox-option">
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                        </div>
+                        <div className="form-checkbox-option">
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                        </div>
+                        <div className="form-checkbox-option">
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                        </div>
+                        <div className="form-checkbox-option">
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                        </div>
+                        <div className="form-checkbox-option">
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                            <input className="form-checkbox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                            <label className="form-checkbox-label" for="vehicle1"> I have a bike</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="quote-form-col">
                     <label className="form-label" id="message-label" htmlFor="message">Message:</label>
                 </div>
                 <div className="quote-form-col">
-                    <textarea className="form-input" id="message-input" name="message"></textarea>
+                    <textarea className="form-input" id="message-input" name="message" required></textarea>
                 </div>
                 <div className="quote-form-col">
                     <button id="quote-submit-btn" type="submit">Submit</button>
