eda028179aa02ed63c912b12546c3f8979352186
James Geib <james@zuse.com>
2024-06-12 14:09:44 -0400

    implemented initial structure and styling for Quote page

--- client/src/pages/Quotes/index.js
+++ client/src/pages/Quotes/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Icon, Header } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import QuoteForm from "../../components/QuoteForm";
+
+import "./style.css";
 
 const Quotes = (props) => {
     return (
         <>
-            <h1>Quotes</h1>
+            <div id="quote-container">
+                <div id="quote">
+                    <div className="quote-col">
+                        <Header className="quote-header" id="quote-header1" as="h1">Contact Information</Header>
+                        <div id="quote-info">
+                            <p><Icon id="phone-icon" name="phone" /> (425) 334-4040</p>
+                            <p><Icon id="mail-icon" name="mail" /> <a id="mail-link" href="mailto:tyler@wzexcavting.com">tyler@wzexcavting.com</a></p>
+                            <p><Icon id="location-icon" name="map marker alternate" /> 1323 Ave D #1129, Snohomish, WA 98290</p>
+                        </div>
+                    </div>
+                    <div className="quote-col">
+                        <Header className="quote-header" as="h1">Get A Free Quote Today!</Header>
+                        <QuoteForm />
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
