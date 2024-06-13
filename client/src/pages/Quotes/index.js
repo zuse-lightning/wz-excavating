78ea5156e229f2c65c74e51773842866c42e6314
James Geib <james@zuse.com>
2024-06-13 11:14:49 -0400

    corrected phone number on Contact and Quote pages, added text to left column content of Quote page

--- client/src/pages/Quotes/index.js
+++ client/src/pages/Quotes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Icon, Header } from "semantic-ui-react";
+import { Icon, Header, List } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import QuoteForm from "../../components/QuoteForm";
 
@@ -11,11 +11,19 @@ const Quotes = (props) => {
             <div id="quote-container">
                 <div id="quote">
                     <div className="quote-col">
-                        <Header className="quote-header" id="quote-header1" as="h1">Contact Information</Header>
                         <div id="quote-info">
-                            <p><Icon id="phone-icon" name="phone" /> (425) 334-4040</p>
-                            <p><Icon id="mail-icon" name="mail" /> <a id="mail-link" href="mailto:tyler@wzexcavting.com">tyler@wzexcavting.com</a></p>
-                            <p><Icon id="location-icon" name="map marker alternate" /> 1323 Ave D #1129, Snohomish, WA 98290</p>
+                            <p className="quote-text">
+                                Get a free quote on any of our listed services, and feel free to mix and match based on your particular needs.
+                                We are more than happy to accomodate in order to give you the best possible service.
+                            </p>
+                            <br />
+                            <p className="quote-text">
+                                If you have any questions or concerns, please feel free to reach out to us at <a href="tel:+12037388106">(203) 738-8106</a> or fill out the
+                                quote form on this page.
+                            </p>
+                            <List id="quote-list">
+                                
+                            </List>
                         </div>
                     </div>
                     <div className="quote-col">
