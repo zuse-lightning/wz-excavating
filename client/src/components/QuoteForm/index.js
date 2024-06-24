2f0efd8d6b67acfb5b064649da161c032b96efdf
James Geib <james@zuse.com>
2024-06-24 11:02:22 -0400

    implemented functionality to QuoteForm

--- client/src/components/QuoteForm/index.js
+++ client/src/components/QuoteForm/index.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import "./style.css";
 
 const QuoteForm = (props) => {
 
+    const QUOTE_ENDPOINT = "https://www.formbackend.com/f/9a3b5607d502b76d";
+    const [submitted, setSubmitted] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const quoteForm = document.getElementById("quote-form");
+        const formData = new FormData(quoteForm);
+
+        const response = await fetch(QUOTE_ENDPOINT, {
+            method: "POST",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+                "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+                "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+            },
+            body: formData
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error("Form response was not ok");
+            }
+            setSubmitted(true);
+        }).catch(error => {
+            e.target.submit();
+        });
+    };
+
+    if (submitted) {
+        navigate("/");
+    }
+
     return (
         <>
-            <form id="quote-form">
+            <form
+                action={QUOTE_ENDPOINT}
+                onSubmit={handleSubmit}
+                method="POST" 
+                id="quote-form"
+            >
                 <div className="quote-form-col">
                     <label className="form-label" id="name-label" htmlFor="name">Name:</label>
                 </div>
@@ -44,43 +83,43 @@ const QuoteForm = (props) => {
                     <div className="form-input checkbox-list">
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="top-soil" value="Top Soil" />
-                            <label className="form-checkbox-label" for="top-soil"> Top Soil</label>
+                            <label className="form-checkbox-label" htmlFor="top-soil"> Top Soil</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="gravel" value="Gravel" />
-                            <label className="form-checkbox-label" for="gravel"> Gravel</label>
+                            <label className="form-checkbox-label" htmlFor="gravel"> Gravel</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="land-grading" value="Land Grading" />
-                            <label className="form-checkbox-label" for="land-grading"> Land Grading</label>
+                            <label className="form-checkbox-label" htmlFor="land-grading"> Land Grading</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="excavation" value="Excavation" />
-                            <label className="form-checkbox-label" for="excavation"> Excavation</label>
+                            <label className="form-checkbox-label" htmlFor="excavation"> Excavation</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="drainage" value="Drainage Systems & Trenches" />
-                            <label className="form-checkbox-label" for="drainage"> Drainage Systems & Trenches</label>
+                            <label className="form-checkbox-label" htmlFor="drainage"> Drainage Systems & Trenches</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="patios" value="Patios, Walks & Retaining Walls" />
-                            <label className="form-checkbox-label" for="patios"> Patios, Walks & Retaining Walls</label>
+                            <label className="form-checkbox-label" htmlFor="patios"> Patios, Walks & Retaining Walls</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="lawn-care" value="Lawn Care" />
-                            <label className="form-checkbox-label" for="lawn-care"> Lawn Care</label>
+                            <label className="form-checkbox-label" htmlFor="lawn-care"> Lawn Care</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="site-prep" value="Site Prep" />
-                            <label className="form-checkbox-label" for="site-prep"> Site Prep</label>
+                            <label className="form-checkbox-label" htmlFor="site-prep"> Site Prep</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="driveways" value="Driveways" />
-                            <label className="form-checkbox-label" for="driveways"> Driveways</label>
+                            <label className="form-checkbox-label" htmlFor="driveways"> Driveways</label>
                         </div>
                         <div className="form-checkbox-option">
                             <input className="form-checkbox" type="checkbox" name="landscape" value="Landscape Architecture" />
-                            <label className="form-checkbox-label" for="landscape"> Architecture</label>
+                            <label className="form-checkbox-label" htmlFor="landscape"> Architecture</label>
                         </div>
                     </div>
                 </div>
@@ -91,7 +130,10 @@ const QuoteForm = (props) => {
                     <textarea className="form-input" id="message-input" name="message" required></textarea>
                 </div>
                 <div className="quote-form-col">
-                    <button id="quote-submit-btn" type="submit" disabled>Submit</button>
+                    <button id="quote-submit-btn" type="submit">Submit</button>
+                </div>
+                <div style={{ textIndent: "-99999px", whiteSpace: "nowrap", overflow: "hidden", position: "absolute" }} aria-hidden="true">
+                    <input type="text" name="_gotcha" tabIndex={-1} autoComplete="off" />
                 </div>
             </form>
         </>
