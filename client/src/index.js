c3b3d59298656431e8865907648d83555f9309b5
James Geib <james@zuse.com>
2024-06-25 12:02:17 -0400

    implemented SEO component for Home page and cleaned up root index.html file

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import ScrollToTop from "./components/ScrollToTop";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const helmetContext = {};
 root.render(
-  <Router>
-    <ScrollToTop />
-    <App />
-  </Router>
+  <HelmetProvider context={helmetContext}>
+    <Router>
+      <ScrollToTop />
+      <App />
+    </Router>
+  </HelmetProvider>
 );
 
 reportWebVitals();
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomeSlideShow from "../../components/HomeSlideShow";
 import AboutUs from "../../components/AboutUs";
 import OurServices from "../../components/OurServices";
+import SEO from "../../components/SEO";
 
 const Home = (props) => {
 
@@ -9,6 +10,14 @@ const Home = (props) => {
 
     return (
         <>
+            <SEO 
+                title="WZ Trucking & Excavating"
+                description="WZ Excavating is a family-owned and operated business located in Snohomish County. We have been serving the area for over 20 years. We specialize in residential and commercial excavation, septic system installation, and site work. Our team is dedicated to providing the highest quality service and customer satisfaction."
+                keywords="WZ Trucking & Excavating, Snohomish, WA, Snohomish County, Top Soil, Gravel, Land Grading, Excavation, Drainage Systems & Trenches, Patio, Walks and Retaining Walls, Lawn Care, Site Preparation, Driveways, Landscape Architecture"
+                type="website"
+                image="https://wz-excavating-bucket.s3.amazonaws.com/screenshots/Home.png"
+                url="https://www.wzexcavating.com"
+            />
             <HomeSlideShow screenWidth={screenWidth} />
             <AboutUs screenWidth={screenWidth} />
             <OurServices screenWidth={screenWidth} />
