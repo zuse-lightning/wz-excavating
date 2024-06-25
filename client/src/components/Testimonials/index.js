e2621d60e28fd5a29b543156e67b446d790a648d
James Geib <james@zuse.com>
2024-06-25 10:25:55 -0400

    readjusted styling for Testimonials Slider for all desktop screens

--- client/src/components/Testimonials/index.js
+++ client/src/components/Testimonials/index.js
@@ -7,14 +7,37 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./style.css";
 
+const NextArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", borderRadius: "50%" }}
+            onClick={onClick}
+        />
+    );
+}
+
+const PrevArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", borderRadius: "50%" }}
+            onClick={onClick}
+        />
+    );
+};
+
 const Testimonials = () => {
 
     const settings = {
-        dots: true,
         infinite: true,
         speed: 500,
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
     };
 
     return (
@@ -23,27 +46,18 @@ const Testimonials = () => {
                 <Header id="testimonials-header" as="h1">Hear From Our Valued Customers</Header>
             </div>
             <div id="testimonials">
-               <Slider {...settings}>
-                {testimonials.map((testimonial, index) => (
-                    <div key={index} className="testimonial-col">
-                        <Icon className="testimonial-icon" name="quote left" size="big" />
-                        <p className="testimonial-text">
-                            {testimonial.review}
-                        </p>
-                        <p className="testimonial-text">{testimonial.name} - {testimonial.location}</p>
-                    </div>
-                ))}
-            </Slider> {/* {testimonials.map((testimonial, index) => (
-                    <div key={index} className="testimonial-col">
-                        <Icon className="testimonial-icon" name="quote left" size="big" />
-                        <p className="testimonial-text">
-                            {testimonial.review}
-                        </p>
-                        <p className="testimonial-text">{testimonial.name} - {testimonial.location}</p>
-                    </div>
-                ))} */}
+                <Slider {...settings}>
+                    {testimonials.map((testimonial, index) => (
+                        <div key={index} className="testimonial-col">
+                            <Icon className="testimonial-icon" name="quote left" size="big" />
+                            <p className="testimonial-text">
+                                {testimonial.review}
+                            </p>
+                            <p className="testimonial-text">{testimonial.name} - {testimonial.location}</p>
+                        </div>
+                    ))}
+                </Slider>
             </div>
-            
         </div>
     );
 };
